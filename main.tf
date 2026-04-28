eb18fc8cffb9b4c2038e5e34b9919dcd0482b765
victoria-101480 <uzuegbunamvictoriaamaka@gmail.com>
2026-04-28 19:00:20 +0000

    Add environment-specific workflows

--- main.tf
+++ main.tf
@@ -1,4 +1,3 @@
-# Simple S3 bucket to demonstrate GitHub Actions automation
 terraform {
   required_providers {
     aws = {
@@ -15,39 +14,8 @@ provider "aws" {
 variable "bucket_suffix" {
   description = "Unique suffix for bucket name (use your initials)"
   type        = string
-  default     = "va"  # CHANGE THIS to your initials!
+  default     = "YOUR_INITIALS"  # CHANGE THIS to your initials!
 }
-
-# S3 Bucket - using fixed name to prevent duplicates
-resource "aws_s3_bucket" "demo" {
-  bucket = "cloudburst-${var.environment}-${var.bucket_suffix}"
-
-  tags = {
-    Name        = "CloudBurst ${var.environment} Bucket"
-    Environment = "${var.environment}"
-    ManagedBy   = "terraform"
-    DeployedBy  = "github-actions"
-    TestTag = "pr-comment-test"
-  }
-}
-
-# Output the bucket name
-output "bucket_name" {
-  description = "Name of the created S3 bucket"
-  value       = aws_s3_bucket.demo.bucket
-}
-
-output "bucket_arn" {
-  description = "ARN of the created S3 bucket"
-  value       = aws_s3_bucket.demo.arn
-}
-
-# Enable versioning on the bucket
-resource "aws_s3_bucket_versioning" "demo" {
-  bucket = aws_s3_bucket.demo.id
-  versioning_configuration {
-    status = "Enabled"
-  }
 
 variable "environment" {
   description = "Deployment environment"
@@ -62,7 +30,22 @@ resource "random_id" "bucket_suffix" {
 resource "aws_s3_bucket" "demo" {
   bucket = "cloudburst-${var.environment}-${var.bucket_suffix}"
 
-  output "environment" {
+  tags = {
+    Name        = "CloudBurst ${var.environment} Bucket"
+    Environment = var.environment
+    ManagedBy   = "terraform"
+    DeployedBy  = "github-actions"
+    TestTag = "pr-comment-test
+    "
+  }
+}
+
+output "bucket_name" {
+  description = "Name of the created S3 bucket"
+  value       = aws_s3_bucket.demo.bucket
+}
+
+output "environment" {
   description = "Deployment environment"
   value       = var.environment
 }
